a6b0361be46e3cb96359cc24a677e917dedc632e
Dmytro Hulid <saindi777@gmail.com>
2024-04-20 15:08:32 +0300

    HM2: refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
-import Header from "./Header/Header.jsx";
-import Content from "./Content/Content.jsx";
+import Input from "./components/Input/Input.jsx";
+import Button from "./components/Button/Button.jsx";
 
 function App() {
   return (
       <div className="wrapper">
-          <Header />
-          <Content />
+
+          <div className="header">
+              <a className='logo' href='/'>Pizza Day</a>
+              <form action="">
+                  <Input type='text' placeholder='Search for the order #'/>
+              </form>
+          </div>
+
+          <div className="content">
+              <h1 className="title">
+                  The best pizza.<br/>
+                  <span className="text-yellow">Straight out of the oven, straight to you.</span>
+              </h1>
+              <p className="sub-title">👋 Welcome! Please start by telling us your name:</p>
+              <form className="login-form">
+                  <Input type='text' placeholder='Your full name'/>
+                  <Button text='Login' type='button'/>
+              </form>
+          </div>
+
       </div>
   )
 }
